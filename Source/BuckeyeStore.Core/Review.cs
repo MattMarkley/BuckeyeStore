6988e34e042b046c33e0f8a52e8b54b5239c0bb4
Matthew Markley <matthewmarkley@Matthews-MacBook-Pro.local>
2017-04-21 17:34:17 -0400

    Second

---
+++ Source/BuckeyeStore.Core/Review.cs
@@ -0,0 +1,17 @@
+
+
+
+namespace BuckeyeStore.Core
+{
+    public class Review
+    {
+        public int Id { get; set; }
+
+        public string Body { get; set; }
+
+        public Rating Rating { get; set; }
+
+
+
+    }
+}
